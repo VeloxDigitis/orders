9d710ed281c3a5ac38b86a24d1607791752133f2
Krzysztof Cybulski <krzysztofpcy@gmail.com>
2018-03-29 15:41:28 +0200

    Post order from form

--- public/javascripts/order.js
+++ public/javascripts/order.js
@@ -1,7 +1,21 @@
 angular.module('orders', [])
-    .controller('order-controller', function($scope) {
+    .controller('order-controller', function($scope, $http) {
 
-        $scope.hello = "Hello world";
+        $scope.items = [];
 
+        $scope.addItem = function () {
+            $scope.items.push({color: $scope.color, size: $scope.size});
+        };
+
+        $scope.removeItem = function (x) {
+            $scope.items.splice(x, 1);
+        };
+
+        $scope.send = function () {
+            $http.post('/orders', {date: new Date(), name: $scope.name, age: parseInt($scope.age)});
+            $scope.items = [];
+            $scope.color = null;
+            $scope.size = null;
+        }
     }
 );
